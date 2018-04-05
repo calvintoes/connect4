75c7dbaa715348fb43c215d69a667c61053cf99f
Calvin <cxt6655@rit.edu>
2018-04-05 02:16:05 -0400

    Final update, fixed winning algorithms and UI bugs

--- connect4.js
+++ connect4.js
@@ -2,14 +2,15 @@ let currPlayer;
 
 const player1 = 1;
 const player2 = 2;
-let player1startColor = "#99e6ff";
-let player2startColor = "#ff9999";
+let player1startColor = "";
+let player2startColor = "";
 
 const numCols = 7;
 const numRows = 6;
 const cellWidth = 85;
 const cellSpacing = 1;
 
+let whoWon;
 
 const container = document.querySelector("#gridContainer");
 let chip = document.querySelectorAll('span.chip');
@@ -19,27 +20,45 @@ let p1Button = document.querySelector("#p1ColorChange");
 let p2Button = document.querySelector("#p2ColorChange");
 
 //----------------Create board--------------------------
+let screenSize;
+
+if (screen.width > 1500)
+	screenSize = screen.width * .34;
+
+else
+	screenSize = screen.width * .26;
+
+container.style.left = screenSize + "px";
+
 const span = document.createElement('span');
 span.className = 'cell';
 
 const cells = [];
 
-	for (let row = 0; row < numRows; row++) {
-		cells.push([]);
-		for (let col = 0; col < numCols; col++) {
-			let cell = span.cloneNode();
-			cell.style.left = `${col * (cellWidth + cellSpacing)}px`;
-			cell.style.top = `${row * (cellWidth + cellSpacing)}px`;
-			container.appendChild(cell);
-			cells[row][col] = cell;
-		}
+for (let row = 0; row < numRows; row++) {
+	cells.push([]);
+	for (let col = 0; col < numCols; col++) {
+		let cell = span.cloneNode();
+		cell.style.left = `${col * (cellWidth + cellSpacing)}px`;
+		cell.style.top = `${row * (cellWidth + cellSpacing)}px`;
+		container.appendChild(cell);
+		cells[row][col] = cell;
+		cells[row][col].innerHTML = "0"
 	}
-//--------------------End Board-------------------------
+}
 
+//2d array in the background
+let array_col = new Array(numCols);
+for (let i = 0; i< numRows;i++){
+	array_col[i] = new Array();
+}
+//--------------------End Board-------------------------
 function startGame() {
+	if (chip[0].style.visibility == "visible" && chip[1].style.visibility == "visible") {
 	//Decides who goes first by random
 	let randNum = (Math.floor(Math.random() * 2) + 1);
 
+	//Will display who goes first with showing the chip over the players name
 	if (randNum == 1) {
 		currPlayer = 1;
 		chip[0].style.visibility = "visible";
@@ -57,29 +76,39 @@ function startGame() {
 	document.getElementById("restartGame").style.visibility = "visible";
 	document.getElementById("startGame").style.visibility = "hidden";
 
+	//sets the color
 	player1startColor = p1changedColor.style.color;
 	player2startColor = p2changedColor.style.color;
 
 	document.getElementById("gridContainer").style.pointerEvents = "auto";
+	}
+	else {
+		alert("Please pick a color!");
+	}
 }
 
+//Button to reset the game
 function restartGame() {
 	p1Button.style.visibility = "visible";
 	p2Button.style.visibility = "visible";
-  document.getElementById("restartGame").style.visibility = "hidden";
+  	document.getElementById("restartGame").style.visibility = "hidden";
 	document.getElementById("startGame").style.visibility = "visible";
 
 	chip[0].style.visibility = "hidden";
 	chip[1].style.visibility = "hidden";
 
+	player1startColor = "";
+	player2startColor = "";
+
 	let cells = document.getElementsByClassName("cellSelected");
-	// debugger;
+	//debugger;
 	while(cells.length){
+		cells[0].innerHTML = "0";
 		cells[0].className = "cell";
+
 	}
+
 	document.getElementById("gridContainer").style.pointerEvents = "none";
-
-
 }
 
 //-----------------------------COLOR Options------------------------------
@@ -101,20 +130,18 @@ function changeColor(array) {
 //changes the chip color depending on which button was pressed
 function changeChipColor (p) {
 	if (p == 1){
-		chip[0].style.visibility = "visible";
 		chip[0].style.backgroundColor = p1changedColor.style.color;
+		chip[0].style.visibility = "visible";
 	}
 
 	else if (p == 2){
-		chip[1].style.visibility = "visible";
 		chip[1].style.backgroundColor = p2changedColor.style.color;
+		chip[1].style.visibility = "visible";
 	}
-
 }
 //-------------------------------------------------------------------------
 
 //add events to the buttons when clicked
-
 p1Button.addEventListener("click", () => {changeChipColor(1); }, false);
 p2Button.addEventListener("click", () => {changeChipColor(2); }, false);
 
@@ -127,30 +154,204 @@ function placeChip (e) {
 	let columnWidth = cellWidth + cellSpacing;
 	let col = Math.floor(mouseX/columnWidth);
 	let row = Math.floor(mouseY/columnWidth);
-	let selectedCell = cells[row][col];
-	selectedCell.className = 'cellSelected';
+	if (isOccupied(row, col) == true)
+		alert("Spot is occupied");
 
-	if (currPlayer == 1) {
-		selectedCell.style.backgroundColor = player1startColor;
-		currPlayer = 2;
-	}
+	else if (!isOccupied(row, col)) {
+		let newRow = check(row, col)
+		let selectedCell = cells[newRow][col];
 
-	else {
-		selectedCell.style.backgroundColor = player2startColor;
-		currPlayer = 1;
+		selectedCell.className = 'cellSelected';
+		console.log(`${col},${row}`);
+
+		if (currPlayer == 1) {
+			selectedCell.innerHTML = "1";
+			selectedCell.style.backgroundColor = player1startColor;
+			currPlayer = 2;
+			chip[0].style.visibility = "hidden";
+			chip[1].style.visibility = "visible";
+				let playerId = cells[row][col].innerHTML;
+				if	(winHorizontally(row,col,col,playerId) == 4){
+					alert("Player 1 won!");
+					restartGame();
+				}
+				if	(positiveDiagonalWin(1,row, col)){
+					alert("Player 1 won!");
+					restartGame();
+				}
+				if	(negativeDiagonalWin(1,row, col)){
+					alert("Player 1 won!");
+					restartGame();
+				}
+				//if	(winDiagonally2(row,row,col,col,playerId) == 4){
+				//	alert("Player 1 won!");
+				//}
+			}
+
+
+		else {
+			selectedCell.innerHTML = "2";
+			selectedCell.style.backgroundColor = player2startColor;
+			currPlayer = 1;
+			chip[0].style.visibility = "visible";
+			chip[1].style.visibility = "hidden";
+			let playerId = cells[row][col].innerHTML;
+				if	(winHorizontally(row,col,col,playerId) == 4){
+					alert("Player 2 won!");
+					restartGame();
+				}
+				if	(positiveDiagonalWin(2,row, col)){
+					alert("Player 2 won!");
+					restartGame();
+				}
+				if	(negativeDiagonalWin(2,row, col)){
+					alert("Player 2 won!");
+					restartGame();
+				}
+				//if	(winDiagonally2(row,row,col,col,playerId) == 4){
+					//alert("Player 2 won!");
+				//}
+			}
+
+
+
+		if (winVertically(row,col) == 1){
+			alert("Player 1 won!");
+			restartGame();
+		}
+
+		else if (winVertically(row,col) == 2){
+			alert("Player 2 won!");
+			restartGame();
+		}
 	}
 }
 
 /*
 Checks to see if the column has other pieces
 */
-function check(col) {
-	//recursive?
+function check(row, col) {
+	for (let i = 5; i > 0; i--) {
+		if (cells[i][col].innerHTML == "0")
+			return i;
+	}
+	return row;
+}
 
-	//go through the column go down the row
-	//check to see if the cell.style.backgroundColor != white;
-		//move down, check again
+function isOccupied(row, col) {
+	if (cells[row][col].innerHTML == "1" || cells[row][col].innerHTML == "2")
+		return true;
+	else if (cells[row][col].innerHTML == "0")
+		return false;
+}
+function winVertically(row, col) {
+	if (cells[row][col].innerHTML != "0" && cells[row + 1][col].innerHTML != "0" &&
+		cells[row + 1][col].innerHTML != "0" && cells[row + 1][col].innerHTML != "0") {
+		if (cells[row][col].innerHTML == "1" &&
+			cells[(row + 1)][col].innerHTML == "1" &&
+			cells[(row + 2)][col].innerHTML == "1" &&
+			cells[(row + 3)][col].innerHTML == "1")
+				return 1;
+		else if (cells[row][col].innerHTML == "2" &&
+			cells[(row + 1)][col].innerHTML == "2" &&
+			cells[(row + 2)][col].innerHTML == "2" &&
+			cells[(row + 3)][col].innerHTML == "2")
+				return 2;
+		else
+			return 0;
+		}
+}
 
-	//go down until a) no more spaces in the column b) current cell is only one free
+function winHorizontally(row, col, col2, playerId) {
+	if ((col < 0 || col > 6 ) || (cells[row][col].innerHTML != playerId)){
+		return 0;
+	}
+	else{
+	return winHorizontally(row, col-1, -1,playerId) + 1 + winHorizontally(row, -1,col+1,playerId);
+	}
+}
+/*
+function winDiagonally(row, row2, col, col2, playerId) {
+	//debugger;
+	if ((col < 0 || col > 6 ) || (row < 0 || row > 5 )|| (cells[row][col].innerHTML != playerId))
+		return 0;
+	else
+	return winDiagonally(row-1, -1, col-1, -1,playerId) + 1 + winDiagonally(-1, row2+1, -1,col+1,playerId);
+}
 
+function winDiagonally2(row, row2, col, col2, playerId) {
+	//debugger;
+	if ((col < 0 || col > 6 ) || (row < 0 || row > 5 ) || (cells[row][col].innerHTML != playerId))
+		return 0;
+
+	else
+		return winDiagonally2(-1, row2+1, -1,col-1,playerId) + 1 + winDiagonally2(row-1, -1, col+1, -1,playerId);
+
+}
+*/
+function winDiagonally(){
+	for(let col = 0; col <= 3; col++){
+		for(let row = 3; row < 5; row++){
+			//debugger;
+			if((cells[row][col].innerHTML != 0) &&
+					(cells[row][col].innerHTML == cells[row-1][col+1].innerHTML) &&
+				 	(cells[row][col].innerHTML == cells[row-2][col+2].innerHTML) &&
+					(cells[row][col].innerHTML == cells[row-3][col+3].innerHTML)){
+				return cells[row][col].innterHTML;
+				}
+				else {
+					return 0;
+				}
+			}
+			for(let row = 0; row < 2; row++){
+				if((cells[row][col].innerHTML != 0) &&
+						(cells[row][col].innerHTML == cells[row+1][col+1].innerHTML) &&
+					 	(cells[row][col].innerHTML == cells[row+2][col+2].innerHTML) &&
+						(cells[row][col].innerHTML == cells[row+3][col+3].innerHTML)){
+					return cells[row][col].innterHTML;
+				}
+				else {
+					return 0;
+				}
+			}
+	}
+}
+
+
+
+function outOfBottomBound(row, col){
+  return (row>5) || (col>5);
+}
+
+function outOfTopBound(row,col){
+  return (row<0) || (col<0);
+}
+
+function recurseSlots(playerTurn, row, col, rowModifier, colModifier){
+  if(stopCount(playerTurn, row, col)) return 0;
+  return recurseSlots(playerTurn, row+rowModifier, col+colModifier, rowModifier, colModifier) + 1;
+}
+
+function stopCount(playerTurn, row, col){
+  if(outOfBottomBound(row,col) || outOfTopBound(row,col)){
+    return true;
+  }
+  return (cells[row][col].innerHTML==null) ? true : cells[row][col].innerHTML!=playerTurn;
+}
+
+function positiveDiagonalWin(playerTurn, row,col){
+	//debugger;
+  return (recurseSlots(playerTurn, row+1, col-1, 1, -1) + 1 + recurseSlots(playerTurn, row-1, col+1, -1, 1)==4) ? true: false;
+}
+
+function negativeDiagonalWin(playerTurn, row, col){
+  return ((recurseSlots(playerTurn, row+1, col+1, 1, 1)+1+recurseSlots(playerTurn, row-1, col-1, -1, -1))==4) ? true: false;
+}
+
+function verticalWin(playerTurn, row, col){
+  return ((recurseSlots(playerTurn, row-1, col, -1, 0)+1+recurseSlots(playerTurn, row+1, col, 1, 0))==4) ? true: false;
+}
+
+function horizontalWin(playerTurn, row, col){
+  return ((recurseSlots(playerTurn, row, col+1, 0, 1)+1+recurseSlots(playerTurn, row, col-1, 0, -1))==4) ? true: false;
 }
